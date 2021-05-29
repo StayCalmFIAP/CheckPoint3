bef12f848c6f22d598b15cc7639af24e5609f749
ShimSooChang <rm88760@fiap.com.br>
2021-05-29 12:53:02 -0300

    mudei aluns id's e chamei a funcao para login

--- JS/scrip.js
+++ JS/scrip.js
@@ -1,6 +1,5 @@
 function Pessoa(nome, senha){
     this.nome = nome,
-    this.email = email,
     this.senha = senha
 }
 
@@ -11,11 +10,10 @@ function Sac(assunto, nome, coment) {
     
 }
 
-var rnome = [];
-var rsenha = [];
-const botaoRgt = document.getElementById('btnEnviarSac');
-botaoRgt.addEventListener('click',function(){
-    
+var rnome = ['Gabriel',];
+var rsenha = ['RM88760',];
+function fazerRegistro(){
+  
     let userName = idTxtUser.value;
     let userPass = idTxtPass.value;
     
@@ -26,57 +24,75 @@ botaoRgt.addEventListener('click',function(){
     
     
     
-});
+    
+}
+function fazerLogon(){
 
-const botaoLgn = document.getElementById('btnEnviarSac');
-botaoLgn.addEventListener('click',function(){
     let controle = true;
     let userName = idTxtUser.value;
-    let userPass = idTxtPass.value;
-    if(!rnome.includes(userName) || !rnome.includes(userPass)){
-        document.alert("Usuario não cadastrado, favor fazer cadastro");
+    let userPass = idtxtSenha.value;
+    const pessoa = new Pessoa(userName,userPass)
+    if(rnome.includes(pessoa.nome) && !rsenha.includes(pessoa.senha) ){
+        alert("Senha incorreta para o usuario");
+        return controle;
+    }else if(rnome.includes(pessoa.nome) && rsenha.includes(pessoa.senha)) {
+        return controle;
+    }else{
+        alert("Usuario não cadastrado, favor fazer cadastro");
         controle = false;
-    }
-    return controle;
+        return controle
+     }
+    
     
 
    
     
     
     
-});
-var dados =['','','',''];
-const botaoSac = document.getElementById('btnEnviarSac');
-botaoSac.addEventListener('click',function(){
-    const listaInputs = document.querySelectorAll('input[type="text"]');
-    
-            let assunto = idtxtAssunto.value;
-           let nome = idtxtNomeSac.value;
-           let comenta = idtxtComentarioSac.value;
+ 
+}
+//funcao de enviar para o sac
+function sac(){
 
-       
-            const sac = new Sac(assunto,nome,comenta);
-                
-                console.log('Assunto  : ' + sac.assunto);
-                console.log('Nome : ' + sac.nome);
-                console.log('Comentario : ' + sac.coment);
-           
+    let assunto = idtxtAssunto.value;
+    let nome = idtxtNomeSac.value;
+    let comenta = idtxtComentarioSac.value;
+    const sac = new Sac(assunto,nome,comenta);
+    console.log('Assunto  : ' + sac.assunto);
+    console.log('Nome : ' + sac.nome);
+    console.log('Comentario : ' + sac.coment);
+    
+    let controle = true;
+    const listaInputs =document.querySelectorAll('input[type="text"]');
+    for (i of listaInputs){
+        if (i.value == ''){
+            alert('Prencha todos os campos antes de enviar');
+            controle = false;
+        }else{
+            alert('Todos os campos foram preenchidos');
+        }
+    }
+    
+    return controle;      
             
              
              
   
-});
+    
+}
 
 function validaCamposForm(){
     let controle = true;
     const listaInputs =document.querySelectorAll('input[type="text"]');
     for (i of listaInputs){
         if (i.value == ''){
-            alert('Prencha todos os campos antes de enviar')
+            alert('Prencha todos os campos antes de enviar');
             controle = false;
+        }else{
+            alert('Todos os campos foram preenchidos');
         }
     }
-    alert('Todos os campos foram preenchidos')
+    
     return controle;
 }
 
